b5dd77ef2848898ea3e9619ad91cc928945f9e27
emann <60099832@udst.edu.qa>
2023-11-27 13:28:25 +0300

    added delete recipe function

--- main.py
+++ main.py
@@ -1,12 +1,57 @@
+from flask import Flask,redirect, url_for, render_template, request, flash
+
+#importing os to get path address of images in static folder
+import os
+
+#for uploading images for add recipe
+from flask_wtf import FlaskForm
+from wtforms import FileField,SubmitField
+from werkzeug.utils import secure_filename
+from wtforms.validators import InputRequired
+
+#for database
 import firebase_admin
 from firebase_admin import credentials, firestore
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY']='supersecretkey'
+app.config['UPLOAD_FOLDER'] = 'static/images'
+
+class UploadFileForm(FlaskForm):
+    file = FileField("File", validators=[InputRequired()])
+    submit= SubmitField("Submit")
+
 
 def db_connection():
     cred = credentials.Certificate("key.json")
     firebase_admin.initialize_app(cred)
     db = firestore.client()
+    
+    return db
+
+def getAllRecipes():
     collection = db.collection("recipes").get()
     recipes_list=[]
     for r in collection:
         recipes_list.append(r.to_dict())
     return recipes_list
+
+db = db_connection()
+recipes_list = getAllRecipes()
+recipe_list_backup = recipes_list
+
+@app.route("/")   #complete
+def view_recipes():
+    recipes= getAllRecipes()
+    #print(recipes)
+    logo = os.path.join(app.config['UPLOAD_FOLDER'], 'logo.jpg')
+    return render_template("home.html",logo=logo,recipe_list=recipes)
+
+@app.route("/deleteRecipe",methods=['GET','POST']) #complete
+def deleteRecipe():
+    logo = os.path.join(app.config['UPLOAD_FOLDER'], 'logo.jpg')
+    recipe_id = request.args.get("id")
+    db.collection("recipes").document(recipe_id).delete()
+    return render_template("delete-confirmation.html",logo=logo)
